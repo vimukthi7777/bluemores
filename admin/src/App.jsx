860a8056756b1a0fa9e53911838d540f62689974
Sahan Vimukthi <svimukthi7777@gmail.com>
2026-03-02 18:46:08 +0530

    admin panel

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Sidebar/>  
+    </>
+  )
+}
+
+export default App
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,11 +5,9 @@ import adminAuth from '../middleware/adminAuth.js';
 
 const productRouter = express.Router();
 
-productRouter.post('/add',upload.fields([{name:'image1', maxCount:1}, {name:'image2', maxCount:1}, {name:'image3', maxCount:1}, {name:'image4', maxCount:1}]), addProduct);
+productRouter.post('/add', adminAuth, upload.fields([{name:'image1', maxCount:1}, {name:'image2', maxCount:1}, {name:'image3', maxCount:1}, {name:'image4', maxCount:1}]), addProduct);
 productRouter.get('/list', listProducts);
 productRouter.post('/single', singleProduct);
 productRouter.post('/remove', adminAuth, removeProduct);
 
 export default productRouter;
-
-// adminAuth, 
