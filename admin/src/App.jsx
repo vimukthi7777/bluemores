9730bec3b21bf8e37038c04dad1d666a64da30f5
Sahan Vimukthi <svimukthi7777@gmail.com>
2026-03-03 15:42:59 +0530

    Product List and Orders Complete

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import Add from './pages/Add';
+import List from './pages/List';
+import Login from './components/Login';
+import Orders from './pages/Orders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
+console.log("Backend URL:", backendUrl);
+export const currency = '$';
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):''); 
+  useEffect(()=>{
+    localStorage.setItem('token', token)
+  }, [token])
+
+
   return (
-    <>
-    <Navbar/>
-    <Sidebar/>  
-    </>
-  )
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer/>
+      {token === '' ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="flex-1 mx-8 my-8 text-gray-700 text-base">
+              <Routes>
+                <Route path="/add" element={<Add token={token}/>} />
+                <Route path="/list" element={<List token={token} />} />
+                <Route path="/order" element={<Orders token={token} />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App
