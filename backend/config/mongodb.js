08f76886f7c46910062354671647fb1892280695
Sahan Vimukthi <svimukthi7777@gmail.com>
2026-03-10 08:07:15 +0530

    finalized system 1

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
-import mongoose from "mongoose"
-=======
 import mongoose from 'mongoose'
->>>>>>> dev
 
-const connectDB = async() => {
-    mongoose.connection.on('connected', ()=>{
+const connectDB = async () => {
+    mongoose.connection.on('connected', () => {
         console.log('Connected to MongoDB')
     })
+
     await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 }
 
