a75fa8cd6f7a29fb9fca3b0f70530ee9dd23720e
Sahan Vimukthi <svimukthi7777@gmail.com>
2026-03-10 08:04:10 +0530

    finalized system 1.0

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,30 @@
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config' 
+import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
-<<<<<<< HEAD
-// import productRouter from './routes/productRoute.js'
-// import cartRouter from './routes/cartRoute.js'
-// import orderRouter from './routes/orderRoute.js'
-=======
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
->>>>>>> dev
 
-//App config
+// App config
 const app = express()
 const port = process.env.PORT || 4000
+
 connectDB();
 connectCloudinary();
+
 // middlewares
 app.use(express.json())
 app.use(cors())
 
 // api endpoints
 app.use('/api/user', userRouter);
-<<<<<<< HEAD
-// app.use('/api/product', productRouter);
-// app.use('/api/cart', cartRouter);
-// app.use('/api/order', orderRouter);
-=======
 app.use('/api/product', productRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
->>>>>>> dev
+
 app.get('/', (req, res) => {
     res.send("API working")
 })
